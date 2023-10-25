a242c063444938840bed130d5265a0a8ffb40d01
kashif khan <mokashifkhan21@gmail.com>
2023-10-25 20:52:57 +0530

    added UI of toolbox

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Menu from "@/components/Menu";
+import Toolbox from "@/components/Toolbox";
 
 
 export default function Home() {
   return (
     <div>
       <Menu></Menu>
+      <Toolbox></Toolbox>
     </div>
   )
 }
