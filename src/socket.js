33214f0979cd15d56d4cf9a32f7d9cb45952abe0
kashif khan <mokashifkhan21@gmail.com>
2023-10-26 18:58:13 +0530

    added backend end-point and deployed

--- src/socket.js
+++ src/socket.js
@@ -1,2 +1,3 @@
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:8000");
+const URL = process.env.NODE_ENV === 'production' ? 'https://sketch-book-server-cbp7.onrender.com' : "http://localhost:8000"
+export const socket = io(URL);
